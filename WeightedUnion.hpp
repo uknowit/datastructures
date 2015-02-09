323b6412b2cba680fc37716cd60e201c1a37b0b3
uknowit <shashikumarhn@gmail.com>
2015-02-09 22:55:29 +0530

    Formatting

--- WeightedUnion.hpp
+++ WeightedUnion.hpp
@@ -12,17 +12,17 @@
 #include <iostream>
 
 class WeightedUnion {
-public:
-	WeightedUnion(int size):m_arrSize(size){init();}
-	~WeightedUnion(){delete [] m_IndexArray;delete[] m_sizeArray;}
-	void m_weightedUnion(const int firstPoint,const int secondPoint);
-	bool m_find(const int firstPoint,const int secondPoint);
-	void m_printArray();
-private:
-	void m_findRoot(int & localFirstPoint);
-	void init();
-	int m_arrSize;
-	int *m_IndexArray,*m_sizeArray;
+	public:
+		WeightedUnion(int size):m_arrSize(size){init();}
+		~WeightedUnion(){delete [] m_IndexArray;delete[] m_sizeArray;}
+		void m_weightedUnion(const int firstPoint,const int secondPoint);
+		bool m_find(const int firstPoint,const int secondPoint);
+		void m_printArray();
+	private:
+		void m_findRoot(int & localFirstPoint);
+		void init();
+		int m_arrSize;
+		int *m_IndexArray,*m_sizeArray;
 };
 
 #endif /* WEIGHTEDUNION_HPP_ */
